8ed51ba1cb349663c4291885e4900bd3ca89e957
Cringomet <marko.budac@gmail.com>
2021-05-05 20:51:48 +0200

    header redegisn

--- client/workout_app/src/components/Header.vue
+++ client/workout_app/src/components/Header.vue
@@ -1,52 +1,59 @@
 <template>
   <div>
-    <b-navbar type="dark" variant="dark">
-      <b-navbar-nav>
-        <b-navbar-nav class="ml-auto">
-          <p>hello</p>
-        </b-navbar-nav>
+
+      <b-navbar toggleable="lg" type="dark" variant="dark">
+        <!-- Left aligned nav items -->
+        <img width="45px" :src="logo_image">
         <b-navbar-brand>Gymloger</b-navbar-brand>
-        <router-link
-            v-if="isAuthenticated"
-            class="nav-link"
-            exact
-            :to="{ name: 'home' }"
-        >
-          Home
-        </router-link>
-        <router-link
-            v-if="!isAuthenticated"
-            class="nav-link"
-            exact
-            :to="{ name: 'register' }"
-        >
-          Register
-        </router-link>
-        <router-link
-            v-if="!isAuthenticated"
-            class="nav-link"
-            exact
-            :to="{ name: 'login' }"
-        >
-          Login
-        </router-link>
-        <router-link
-            v-if="isAuthenticated"
-            class="nav-link"
-            exact
-            :to="{
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <router-link right
+                       v-if="isAuthenticated"
+                       class="nav-link"
+                       exact
+                       :to="{
               name: 'profile',
               params: { username: this.currentUser.username }
             }"
-        >
-          Profile
-        </router-link>
+          >
+            <b-avatar size="1.9rem" variant="info"></b-avatar>
+            Profile
+          </router-link>
+          <router-link right
+              v-if="isAuthenticated"
+              class="nav-link"
+              exact
+              :to="{ name: 'home' }"
+          >
+            <b-icon variant="info"  @click="logout" icon="house-door" aria-hidden="true"></b-icon> Home
+          </router-link>
+          <router-link right
+              v-if="!isAuthenticated"
+              class="nav-link"
+              exact
+              :to="{ name: 'register' }"
+          >
+            Register
+          </router-link>
+          <router-link right
+              v-if="!isAuthenticated"
+              class="nav-link"
+              exact
+              :to="{ name: 'login' }"
+          >
+            Login
+          </router-link>
 
-        <a v-if="isAuthenticated" @click="logout" class="nav-link">Logout</a>
+          <b-button  v-if="isAuthenticated" variant="outline-info" class="mb-2 ml-4">
+            <b-icon  v-if="isAuthenticated"  @click="logout" icon="power" aria-hidden="true"></b-icon> Logout
+          </b-button>
+        </b-navbar-nav>
+      </b-navbar>
 
-      </b-navbar-nav>
-    </b-navbar>
-  </div>
+    </div>
+
+
 </template>
 
 <script>
@@ -55,6 +62,11 @@ import { mapGetters } from "vuex";
 
 export default {
   name: "Header",
+  data(){
+    return{
+      logo_image: require('@/assets/logo.png')
+    }
+  },
   computed: {
     ...mapGetters(["currentUser", "isAuthenticated"])
   },
