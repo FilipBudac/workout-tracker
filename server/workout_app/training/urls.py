ff6aef9a1e1447dff231a2ec5f9a25da713a8728
FilipBudac <filip.budac@gmail.com>
2021-06-13 20:01:43 +0200

    exercises update, delete, get by id

--- server/workout_app/training/urls.py
+++ server/workout_app/training/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from training.views import ExerciseList, BodyPartsCategoryList
+from training.views import ExerciseList, BodyPartsCategoryList, ExerciseView
 
 urlpatterns = [
     path('exercises/', ExerciseList.as_view()),
+    path('exercises/<int:pk>', ExerciseView.as_view()),
     path('categories/', BodyPartsCategoryList.as_view()),
 ]
--- server/workout_app/training/views.py
+++ server/workout_app/training/views.py
@@ -1,13 +1,15 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
-from rest_framework import generics, permissions
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.mixins import DestroyModelMixin
+from rest_framework.permissions import IsAuthenticated
 
 from training.models import Exercise, BodyPartsCategory
 from training.serializers import ExerciseSerializer, BodyPartsCategorySerializer
 
 
-class ExerciseList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+class ExerciseList(ListCreateAPIView):
+    permission_classes = [IsAuthenticated, TokenHasReadWriteScope]
     model = Exercise
     serializer_class = ExerciseSerializer
     queryset = Exercise.objects.all()
@@ -15,8 +17,15 @@ class ExerciseList(generics.ListCreateAPIView):
     filterset_fields = ['name', 'category__name', 'category__id']
 
 
-class BodyPartsCategoryList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+class ExerciseView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated, TokenHasReadWriteScope]
+    model = Exercise
+    serializer_class = ExerciseSerializer
+    queryset = Exercise.objects.all()
+
+
+class BodyPartsCategoryList(ListCreateAPIView):
+    permission_classes = [IsAuthenticated, TokenHasReadWriteScope]
     model = BodyPartsCategory
     serializer_class = BodyPartsCategorySerializer
     queryset = BodyPartsCategory.objects.all()
